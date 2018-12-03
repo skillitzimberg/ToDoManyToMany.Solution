6499294c96a76e411559453464571e644e8ee884
AlexGarcia and Charley McGowan <alexgarcia@>
2018-12-03 10:15:54 -0800

    add refactor to item.cs and connecting with todolist server.

--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -11,8 +11,8 @@ namespace ToDoList
     public Startup(IHostingEnvironment env)
     {
       var builder = new ConfigurationBuilder()
-          .SetBasePath(env.ContentRootPath)
-          .AddEnvironmentVariables();
+      .SetBasePath(env.ContentRootPath)
+      .AddEnvironmentVariables();
       Configuration = builder.Build();
     }
 
@@ -29,15 +29,18 @@ namespace ToDoList
       app.UseMvc(routes =>
       {
         routes.MapRoute(
-          name: "default",
-          template: "{controller=Home}/{action=Index}/{id?}");
-      });
+        name: "default",
+        template: "{controller=Home}/{action=Index}/{id?}");
+        });
 
         app.Run(async (context) =>
         {
           await context.Response.WriteAsync("Something went wrong!");
-        });
-
+          });
+        }
+    public static class DBConfiguration
+    {
+      public static string ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=to_do_list;";
     }
 
   }
