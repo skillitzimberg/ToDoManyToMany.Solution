97b40799977b2515b2c77ec4e288bc18879c74fb
Student <example@example.com>
2018-12-04 13:48:08 -0800

    finish testing for databases

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -42,7 +42,7 @@ namespace ToDoList.Models
       {
         int itemId = rdr.GetInt32(0);
         string itemDescription = rdr.GetString(1);
-        Item newItem = new Item(itemDescription);
+        Item newItem = new Item(itemDescription, itemId);
         allItems.Add(newItem);
       }
       conn.Close();
@@ -71,12 +71,32 @@ namespace ToDoList.Models
     {
       return _id;
     }
-    public static Item Find(int searchId)
-    {
-      Item dummyItem = new Item("dummy item");
-      return dummyItem;
-
-    }
+    public static Item Find(int id)
+  {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM `items` WHERE id = @thisId;";
+      MySqlParameter thisId = new MySqlParameter();
+      thisId.ParameterName = "@thisId";
+      thisId.Value = id;
+      cmd.Parameters.Add(thisId);
+      var rdr = cmd.ExecuteReader() as MySqlDataReader;
+      int itemId = 0;
+      string itemDescription = "";
+      while (rdr.Read())
+      {
+         itemId = rdr.GetInt32(0);
+         itemDescription = rdr.GetString(1);
+      }
+    Item foundItem= new Item(itemDescription, itemId);  // This line is new!
+     conn.Close();
+     if (conn != null)
+     {
+       conn.Dispose();
+     }
+    return foundItem;  // This line is new!
+  }
     public override bool Equals(System.Object otherItem)
     {
       if (!(otherItem is Item))
@@ -86,8 +106,9 @@ namespace ToDoList.Models
       else
       {
         Item newItem = (Item) otherItem;
+        bool idEquality = (this.GetId() == newItem.GetId());
         bool descriptionEquality = (this.GetDescription() == newItem.GetDescription());
-        return (descriptionEquality);
+        return (idEquality && descriptionEquality);
       }
     }
     public void Save()
