d85d2179d18afd268c8d95ea8a9c5e67381e9c54
Student <example@example.com>
2018-12-11 07:57:50 -0800

    update to 12/10 homework.

--- ToDoList/Controllers/CategoryController.cs
+++ ToDoList/Controllers/CategoryController.cs
@@ -13,11 +13,7 @@ namespace ToDoList.Controllers
       List<Category> allCategories = Category.GetAll();
       return View(allCategories);
     }
-    [HttpGet("/categories/new")]
-    public ActionResult New()
-    {
-      return View();
-    }
+
     [HttpPost("/categories")]
     public ActionResult Create(string categoryName)
     {
@@ -26,30 +22,34 @@ namespace ToDoList.Controllers
       List<Category> allCategories = Category.GetAll();
       return View("Index", allCategories);
     }
+
+    [HttpGet("/categories/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
     [HttpGet("/categories/{id}")]
     public ActionResult Show(int id)
     {
       Dictionary<string, object> model = new Dictionary<string, object>();
       Category selectedCategory = Category.Find(id);
       List<Item> categoryItems = selectedCategory.GetItems();
+      List<Item> allItems = Item.GetAll();
       model.Add("category", selectedCategory);
-      model.Add("items", categoryItems);
+      model.Add("categoryItems", categoryItems);
+      model.Add("allItems", allItems);
       return View(model);
     }
-    [HttpPost("/categories/{categoryId}/items")]
-    public ActionResult Create(int categoryId, string itemDescription)
-    {
-      Dictionary<string, object> model = new Dictionary<string, object>();
-      Category foundCategory = Category.Find(categoryId);
-      Item newItem = new Item(itemDescription, categoryId);
-      newItem.Save();
-      
-      List<Item> categoryItems = foundCategory.GetItems();
-      model.Add("items", categoryItems);
-      model.Add("category", foundCategory);
-      return View("Show", model);
-    }
 
+    [HttpPost("categories/{categoryId}/itmes/new")]
+    public ActionResult AddItem(int categoryId, int itemId)
+    {
+      Category category = Category.Find(categoryId);
+      Item item = Item.Find(itemId);
+      category.AddItem(item);
+      return RedirectToAction("Show", new { id = categoryId });
+    }
 
   }
 }
--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -7,50 +7,79 @@ namespace ToDoList.Controllers
 {
   public class ItemsController : Controller
   {
+    [HttpGet("/items")]
+    public ActionResult Index()
+    {
+      List<Item> allItems = Item.GetAll();
+      return View(allItems);
+    }
 
-    [HttpGet("/categories/{categoryId}/items/new")]
+    [HttpGet("/items/new")]
     public ActionResult New(int categoryId)
     {
-       Category category = Category.Find(categoryId);
-       return View(category);
+       return View(categoryId);
     }
-    [HttpGet("/categories/{categoryId}/items/{itemId}")]
+
+    [HttpPost("/items")]
+    public ActionResult Create(string description)
+    {
+      Item newItem = new Item(description);
+      newItem.Save();
+      List<Item> allItems = Item.GetAll();
+      return View("Index", allItems);
+    }
+
+    [HttpGet("/items/{itemId}")]
     public ActionResult Show(int categoryId, int itemId)
     {
-      Item item = Item.Find(itemId);
       Dictionary<string, object> model = new Dictionary<string, object>();
-      Category category = Category.Find(categoryId);
-      model.Add("item", item);
-      model.Add("category", category);
+      Item selectedItem = Item.Find(itemId);
+      List<Category> itemCategories = selectedItem.GetCategories();
+      List<Category> allCategories = Category.GetAll();
+      model.Add("selectedItem", selectedItem);
+      model.Add("itemCategories", itemCategories);
+      model.Add("allCategories", allCategories);
       return View(model);
     }
-    [HttpPost("/items/delete")]
-    public ActionResult DeleteAll()
+
+    [HttpPost("/items/{itemId}/categories/new")]
+    public ActionResult AddCategory(int itemId, int categoryId)
     {
-      Item.ClearAll();
-      return View();
-    }
-    [HttpGet("/categories/{categoryId}/items/{itemId}/edit")]
-    public ActionResult Edit(int categoryId, int itemId)
-    {
-      Dictionary<string, object> model = new Dictionary<string, object>();
+      Item item = Item.Find(categoryId);
       Category category = Category.Find(categoryId);
-      model.Add("category", category);
-      Item item = Item.Find(itemId);
-      model.Add("item", item);
-      return View(model);
+      item.AddCategory(category);
+      return RedirectToAction("Show", new { id = itemId });
     }
-    [HttpPost("/categories/{categoryId}/items/{itemId}")]
-    public ActionResult Update(int categoryId, int itemId, string newDescription)
-    {
-      Item item = Item.Find(itemId);
-      item.Edit(newDescription);
-      Dictionary<string, object> model = new Dictionary<string, object>();
-      Category category = Category.Find(categoryId);
-      model.Add("category", category);
-      model.Add("item", item);
-      return View("Show", model);
-    }
+    //
+    // [HttpPost("/items/delete")]
+    // public ActionResult DeleteAll()
+    // {
+    //   Item.ClearAll();
+    //   return View();
+    // }
+    //
+    // [HttpGet("/categories/{categoryId}/items/{itemId}/edit")]
+    // public ActionResult Edit(int categoryId, int itemId)
+    // {
+    //   Dictionary<string, object> model = new Dictionary<string, object>();
+    //   Category category = Category.Find(categoryId);
+    //   model.Add("category", category);
+    //   Item item = Item.Find(itemId);
+    //   model.Add("item", item);
+    //   return View(model);
+    // }
+    //
+    // [HttpPost("/categories/{categoryId}/items/{itemId}")]
+    // public ActionResult Update(int categoryId, int itemId, string newDescription)
+    // {
+    //   Item item = Item.Find(itemId);
+    //   item.Edit(newDescription);
+    //   Dictionary<string, object> model = new Dictionary<string, object>();
+    //   Category category = Category.Find(categoryId);
+    //   model.Add("category", category);
+    //   model.Add("item", item);
+    //   return View("Show", model);
+    // }
 
   }
 }
